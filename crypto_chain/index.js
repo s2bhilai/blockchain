bd7df786af7923259d8003f10d49cd41ffa39757
Subin <subinbs4a@gmail.com>
2023-07-14 10:11:40 +0300

    api network implemetation completd

--- crypto_chain/index.js
+++ crypto_chain/index.js
@@ -1,5 +1,6 @@
 const bodyParser = require("body-parser");
 const express = require("express");
+const request = require("request");
 const Blockchain = require("./blockchain");
 const PubSub = require("./pubsub");
 
@@ -7,9 +8,12 @@ const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({ blockchain });
 
-setTimeout(() => {
-  pubsub.broadcastChain();
-}, 1000);
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
+
+// setTimeout(() => {
+//   pubsub.broadcastChain();
+// }, 1000);
 
 app.use(bodyParser.json());
 
@@ -22,10 +26,25 @@ app.post("/api/mine", (req, res) => {
 
   blockchain.addBlock({ data });
 
+  pubsub.broadcastChain();
+
   res.redirect("/api/blocks");
 });
 
-const DEFAULT_PORT = 3000;
+const syncChains = () => {
+  request(
+    { url: `${ROOT_NODE_ADDRESS}/api/blocks` },
+    (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const rootChain = JSON.parse(body);
+
+        console.log(`replace chain on sync with`, rootChain);
+        blockchain.replaceChain(rootChain);
+      }
+    }
+  );
+};
+
 let PEER_PORT;
 
 if (process.env.GENERATE_PEER_PORT === "true") {
@@ -33,4 +52,10 @@ if (process.env.GENERATE_PEER_PORT === "true") {
 }
 
 const PORT = PEER_PORT || DEFAULT_PORT;
-app.listen(PORT, () => console.log(`listening at localhost: ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`listening at localhost: ${PORT}`);
+
+  if (PORT !== DEFAULT_PORT) {
+    syncChains();
+  }
+});
